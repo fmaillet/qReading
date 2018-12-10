da3d5bf601d019ae6ee9bb29c39427db1af4df55
FRED-PC\Fred <f.maillet@wanadoo.fr>
2018-12-10 21:13:14 +0100

    Ajoutez des fichiers projet.

---
+++ qReading/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace qReading
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.birthDate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.calButton = new System.Windows.Forms.Button();
+            this.speechButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.aideToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // aideToolStripMenuItem
+            // 
+            this.aideToolStripMenuItem.Name = "aideToolStripMenuItem";
+            this.aideToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.aideToolStripMenuItem.Text = "Aide";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date de naissance :";
+            // 
+            // birthDate
+            // 
+            this.birthDate.CustomFormat = "dd MMM yyyy";
+            this.birthDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.birthDate.Location = new System.Drawing.Point(267, 40);
+            this.birthDate.Name = "birthDate";
+            this.birthDate.Size = new System.Drawing.Size(116, 20);
+            this.birthDate.TabIndex = 2;
+            this.birthDate.Value = new System.DateTime(2010, 1, 1, 0, 0, 0, 0);
+            this.birthDate.ValueChanged += new System.EventHandler(this.birthDate_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age :";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.ageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageLabel.Location = new System.Drawing.Point(267, 75);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(139, 16);
+            this.ageLabel.TabIndex = 4;
+            this.ageLabel.Text = "8 ans 3 mois (12 jours)";
+            // 
+            // calButton
+            // 
+            this.calButton.Location = new System.Drawing.Point(561, 46);
+            this.calButton.Name = "calButton";
+            this.calButton.Size = new System.Drawing.Size(134, 33);
+            this.calButton.TabIndex = 5;
+            this.calButton.Text = "EyeTracker Calibration";
+            this.calButton.UseVisualStyleBackColor = true;
+            this.calButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // speechButton
+            // 
+            this.speechButton.Location = new System.Drawing.Point(561, 111);
+            this.speechButton.Name = "speechButton";
+            this.speechButton.Size = new System.Drawing.Size(134, 33);
+            this.speechButton.TabIndex = 6;
+            this.speechButton.Text = "Synthèse vocale";
+            this.speechButton.UseVisualStyleBackColor = true;
+            this.speechButton.Click += new System.EventHandler(this.speechButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.speechButton);
+            this.Controls.Add(this.calButton);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.birthDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "qReading";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aideToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker birthDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Button calButton;
+        private System.Windows.Forms.Button speechButton;
+    }
+}
+
---
+++ qReading/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tobii.Interaction;
+using Tobii.Interaction.Framework;
+using MySql.Data.MySqlClient;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace qReading
+{
+    public partial class mainForm : Form
+    {
+        public List<string> words;
+        //Boolean connected = false;
+        string macAddr = "";
+        string titre = "";
+        string name = "";
+        string prenom = "";
+        Thread autoConnect;
+
+        string MyConnectionString = "Server=fredericmaillet.fr;Database=fmaillet_professionnels;Uid=fmaillet_fredo;Pwd=mastercog;";
+
+        static private Host eyeX = new Host();
+        //FixationDataStream fixationDataStream;
+        //private static HeadPoseStream _headPoseStream;
+
+        SpeechSynthesizer synth;
+        SpeechRecognitionEngine _recognizer;
+        PromptBuilder consignes, falseRecog;
+        
+        public mainForm()
+        {
+            InitializeComponent();
+            this.Text = "qReading - MODE DEMO (NON CONNECTE)";
+
+            string resource_data = Properties.Resources.mots;
+            words = resource_data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            System.Console.WriteLine(words[0]);
+
+            //Get MacAddress
+            
+            foreach  (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                macAddr = string.Join("-", nic.GetPhysicalAddress().GetAddressBytes().Select(b => b.ToString("X2")));
+                Console.WriteLine("macAddress: " + macAddr);
+                break;  //get only first one
+            }
+            //Launch autoConnect thread
+            autoConnect = new Thread(new ThreadStart(autoConnectThread));
+            autoConnect.Start();
+
+            /*//Test mySQL connection
+            MySqlConnection connection = new MySqlConnection(MyConnectionString);
+            connection.Open();
+            MySqlCommand cmd;
+            MySqlDataReader reader = null;
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "select * from Pro where MACADR = '" + macAddr + "'";
+                Console.WriteLine(cmd.CommandText);
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    titre = reader.GetString("TITRE");
+                    name = reader.GetString("NOM");
+                    prenom = reader.GetString("PRENOM");
+                    connectedToServer(true);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("mySql error");
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("mySql com open");
+                    connection.Close();
+                }
+            }*/
+
+            //Initialize speech synth
+            synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+
+            //Initializing eyeX
+            eyeX = new Host();
+            var userPresenceStateObserver = eyeX.States.CreateUserPresenceObserver();
+            userPresenceStateObserver.WhenChanged(userPresenceState =>
+            {
+                if (userPresenceState.IsValid)
+                    switch (userPresenceState.Value)
+                    {
+                        case UserPresence.Present:
+                            Console.WriteLine("User is present");
+                            allowEyeTrack(true);
+                            break;
+
+                        default:
+                            Console.WriteLine("User is not present");
+                            allowEyeTrack(false);
+                            break;
+                    }
+            });
+        }
+
+        //Observable connected boolean var
+        private Boolean _myConnected = false;
+        delegate void StringArgReturningVoidDelegate(string text);
+        public Boolean MyConnected
+        {
+            get { return _myConnected; }
+            set
+            {
+                _myConnected = value;
+                if (_myConnected == true)
+                {
+                    this.SetText ("qReading - Connecté : " + titre + " " + prenom + " " + name);
+                }
+                else
+                {
+                    this.SetText ("qReading - MODE DEMO (NON CONNECTE)");
+                }
+            }
+        }
+
+        //Just to set the form title thread safe
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.Text = text;
+            }
+        }
+
+        //Auto Connect thread from MacAddress
+        private void autoConnectThread()
+        {
+            //Test mySQL connection
+            MySqlConnection connection = new MySqlConnection(MyConnectionString);
+            connection.Open();
+            MySqlCommand cmd;
+            MySqlDataReader reader = null;
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "select * from Pro where MACADR = '" + macAddr + "'";
+                Console.WriteLine(cmd.CommandText);
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    titre = reader.GetString("TITRE");
+                    name = reader.GetString("NOM");
+                    prenom = reader.GetString("PRENOM");
+                    MyConnected = true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("mySql error");
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("mySql com open");
+                    connection.Close();
+                }
+            }
+        }
+
+
+        //Quit application (from menu)
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        //Allow eye-tracking
+        private void allowEyeTrack (Boolean b)
+        {
+            Invoke(new Action(() =>
+            {
+                calButton.Enabled = b;
+            }));
+            
+        }
+
+        //Compute age from birth date
+        private void birthDate_ValueChanged(object sender, EventArgs e)
+        {
+            //check for today max
+            DateTime bd = this.birthDate.Value.Date;
+            DateTime dt = DateTime.Today;
+            if (DateTime.Compare(bd, dt) >= 0)
+            {
+                birthDate.Value = dt;
+                return;
+            }
+
+            int y = this.birthDate.Value.Date.Year;
+            int m = this.birthDate.Value.Date.Month;
+            int d = this.birthDate.Value.Date.Day;
+            int dd = DateTime.Today.Day;
+            int mm = DateTime.Today.Month;
+            int yy = DateTime.Today.Year;
+
+            // Calc age day
+            if (dd<d)
+            {
+                dd = dd + 30;
+                mm = mm - 1;
+            }
+            int ad = dd - d;
+            //calc age month
+            if (mm < m)
+            {
+                mm = mm + 1;
+                yy = yy - 1;
+            }
+            int am = mm - m;
+            //calc age year
+            int ay = yy - y;
+            //set label
+            string st = ay + " ans " + am + " mois (" + ad + " jours)";
+            this.ageLabel.Text = st;
+        }
+
+        //Launch eye-tracker calibration
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //eyeX.Context.LaunchConfigurationTool(ConfigurationTool.RetailCalibration, (data) => { });
+            eyeX.Context.LaunchConfigurationTool(ConfigurationTool.Recalibrate, (data) => { });
+        }
+
+        //Check for speech synthesis
+        private void speechButton_Click(object sender, EventArgs e)
+        {
+            consignes = new PromptBuilder();
+
+            consignes.StartSentence();
+            consignes.AppendText("Ceci est un essai du système de synthèse vocale.");
+            //consignes.StartStyle(new PromptStyle(PromptEmphasis.Moderate));
+            //consignes.AppendText("système de synthèse vocale");
+            //consignes.EndStyle();
+            //consignes.AppendText(" le plus vite possible.");
+            consignes.EndSentence();
+
+            synth.SpeakAsync(consignes);
+        }
+    }
+}
